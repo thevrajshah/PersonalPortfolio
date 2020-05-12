9a43f15b6d4562efd199c859f1ccdb8682b87736
Vraj Shah <vshah0090@gmail.com>
2020-05-12 20:39:36 +0530

    Colours Neutralized

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,8 +10,11 @@ class Home extends Component {
       <React.Fragment>
         <Hero />
         <Section>
-          <h1>Welcome my Website.</h1>
-          This is the place where I put up my blah blah blah
+          <h1>Welcome my Website.</h1>I create this place in order to put
+          together all my work, thoughts & information all in one place for no
+          specific reason (Just wanted to have something like this). Anyone who
+          visits this site can get into my mind once I'm completely done with
+          the building process.
         </Section>
       </React.Fragment>
     );
