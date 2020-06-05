5fb95def1c0e52813cfab8c51aecaca5a307a931
Vraj Shah <vshah0090@gmail.com>
2020-06-05 23:17:15 +0530

    Router Added

---
+++ client/src/containers/Contact.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./containers.scss";
+import Section from "../UI/Section";
+
+class Contact extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Section>
+          <h1>About</h1>
+        </Section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contact;
