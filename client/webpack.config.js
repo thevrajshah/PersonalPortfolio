bbd4c905f94725f888b9d8c49099f79840050532
Vraj Shah <vshah0090@gmail.com>
2020-06-10 22:49:57 +0530

    Finally webpack Fixed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,51 +1,84 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const port = process.env.PORT || 3000;
+/* const settings = {
+  distPath: path.join(__dirname, "build"),
+  srcPath: path.join(__dirname, "src"),
+};
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/build"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js(x*)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+function srcPathExtend(subpath) {
+  return path.join(settings.srcPath, subpath);
+} */
+
+module.exports = (env, options) => {
+  const isDevMode = options.mode === "development";
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "/build"),
+      filename: "bundle.js",
+    },
+    devtool: isDevMode ? "source-map" : false,
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js(x*)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
         },
-      },
-      {
-        test: /\.(s*)css$/,
-        exclude: /node_modules/,
-        loader: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "resolve-url-loader",
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: isDevMode,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "file-loader",
             options: {
-              limit: 20000,
+              name: "fonts/[name].[ext]",
             },
           },
-        ],
-      },
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|ico)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "assets/",
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      /* new CleanWebpackPlugin([settings.distPath], {
+        verbose: true,
+      }), */
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  devServer: {
-    open: true,
-    hot: true,
-    port: port,
-    compress: true,
-  },
+  };
 };
