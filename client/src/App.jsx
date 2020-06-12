4479ab45f5c758f50f1f48de0f960571e521a7be
thevrajshah <42384591+thevrajshah@users.noreply.github.com>
2020-06-13 00:51:04 +0530

    added Hide NavBar on scrollDown

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,44 @@ import { Home, About, Contact } from "./containers/Pages";
 import Footer from "./components/Footer";
 
 function App() {
+
+state = { hidden: false };
+
+  constructor(props) {
+    super(props);
+
+    // Bind the function to this component, so it has access to this.state
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentWillMount() {
+    // When this component mounts, begin listening for scroll changes
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // If this component is unmounted, stop listening
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(e) {
+    let lastScrollTop = 0;
+    const currentScrollTop = navbar.scrollTop;
+
+    // Set the state of hidden depending on scroll position
+    // We only change the state if it needs to be changed
+    if (!this.state.hidden && currentScrollTop > lastScrollTop) {
+      this.setState({ hidden: true });
+    } else if(this.state.hidden) {
+      this.setState({ hidden: false });
+    }
+    lastScrollTop = currentScrollTop;
+  }
+
   return (
     <React.Fragment>
       <Router>
-        <NavBar />
+        <NavBar hidden={this.state.hidden} />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
