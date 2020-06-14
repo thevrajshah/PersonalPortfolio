87f7b23ecefb8b38728f524415d49b3975fe103a
Vraj Shah <vshah0090@gmail.com>
2020-06-14 22:38:05 +0530

    minor changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://thevrajshah:vraj123@mern-blog-2fsep.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://thevrajshah:Vraj123@mern-blog-av3le.mongodb.net/blogs?retryWrites=true&w=majority",
 };
