c48e476f64ed589d6dcb862b26e90dd636880e1e
Vraj Shah <vshah0090@gmail.com>
2020-06-29 12:43:16 +0530

    loader Added

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./components.scss";
+import { Section } from "../containers/Section";
 
 class Loader extends Component {
   render() {
     return (
-      <div className="loader">
-        <i className="fa fa-refresh" aria-hidden="true"></i>
-      </div>
+      <Section>
+        <div className="loader">
+          <i className="fa fa-refresh" aria-hidden="true"></i>
+        </div>
+      </Section>
     );
   }
 }
