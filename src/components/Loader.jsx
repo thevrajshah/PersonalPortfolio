d41f5cfbc728520265532ce1d61f45961f6a1e32
thevrajshah <42384591+thevrajshah@users.noreply.github.com>
2020-07-03 03:47:21 +0530

    gave up on file-loader 3.0

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ class Loader extends Component {
   render() {
     return (
       <div className='loader'>
-        <img src='/public/assets/dino.gif' alt='' />
+        <img src='https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/Dino_non-birthday_version.gif' alt='' />
       </div>
     );
   }
