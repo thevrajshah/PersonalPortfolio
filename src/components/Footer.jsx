01cb4b593ab3dc7518ea7bc718a234c02b33c88c
Vraj Shah <vshah0090@gmail.com>
2020-07-12 17:37:10 +0530

    scroll-to-top button added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,29 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <div id='createdWith'>
-          Created with &nbsp;
-          <span
-            className='fas fa-heart'
-            style={{ fontSize: "14px", color: "#dc143c" }}
-          />
-          &nbsp; & &nbsp;
-          <span
-            className='fab fa-react'
-            style={{ fontSize: "15px", color: "#0ad3fd" }}
-          />
-          .
-        </div>
-        <button id='toTop' onClick={() => window.scrollTo(0, 0)}>
-          <i className='fa fa-chevron-up' aria-hidden='true' />
-        </button>
-        <div id='copyright'>
-          © 2020 <b>VrajShah</b>.
-        </div>
+        <section>
+          <button id='toTop' onClick={() => window.scrollTo(0, 0)}>
+            <i className='fa fa-angle-up' aria-hidden='true' />
+          </button>
+          <div id='credits'>
+            <div id='createdWith'>
+              Created with &nbsp;
+              <span
+                className='fas fa-heart'
+                style={{ fontSize: "14px", color: "#dc143c" }}
+              />
+              &nbsp; & &nbsp;
+              <span
+                className='fab fa-react'
+                style={{ fontSize: "15px", color: "#0ad3fd" }}
+              />
+              .
+            </div>
+            <div id='copyright'>
+              © 2020 <b>VrajShah</b>.
+            </div>
+          </div>
+        </section>
       </footer>
     );
   }
