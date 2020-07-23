f44eed70b11c8a02d19749d29342873e0aa3763e
Vraj Shah <vshah0090@gmail.com>
2020-07-23 12:50:00 +0530

    info updated

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -19,12 +19,12 @@ export default class About extends Component {
           <h1 style={{ fontSize: "4rem" }}>about me</h1>
           <h2>I design & create stuff on the Web.</h2>
           <p>
-            I'm an self-taught, Rookie Developer based in Gujarat, India.
-            Through constant learning and creation, I try to produce aesthetic
-            software at my best. I often contribute to the open-source
-            community. Other than that I enjoy exploring general trivia via
-            podcasts and youtube videos. Also, I love movies especially the
-            horror and thriller ones.
+            I'm a Rookie Developer based in Gujarat, India. Through constant
+            learning and creation, I try to produce aesthetic software at my
+            best. I often contribute to the open-source community. Other than
+            that I enjoy exploring general trivia via podcasts and youtube
+            videos. Also, I love movies especially the horror and thriller ones.{" "}
+            <br /> Have fun exploring the site.
           </p>
           <a
             href='https://twitter.com/thevrajshah?ref_src=twsrc%5Etfw'
